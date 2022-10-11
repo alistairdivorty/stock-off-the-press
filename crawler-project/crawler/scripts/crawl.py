2772f9ddc4c55d9887df92ba811363b423466b47
Alistair Divorty <alistair_divorty@hotmail.com>
2022-10-11 16:12:13 +0100

    Containerize crawler

---
+++ crawler-project/crawler/scripts/crawl.py
@@ -0,0 +1,15 @@
+from twisted.internet import reactor
+from scrapy.crawler import CrawlerRunner
+from scrapy.utils.project import get_project_settings
+from scrapy.utils.log import configure_logging
+from crawler.spiders.ft import FtSpider
+
+configure_logging({"LOG_FORMAT": "%(levelname)s: %(message)s"})
+
+settings = get_project_settings()
+
+runner = CrawlerRunner(settings)
+
+d = runner.crawl(FtSpider)
+d.addBoth(lambda _: reactor.stop())
+reactor.run()
