c75817b74d6a4df03c0bbbb5d5ed4f222a8069f7
alistairdivorty <alistair_divorty@hotmail.com>
2023-01-12 15:53:49 +0000

    Improve responsiveness

--- web-frontend/components/Hero.tsx
+++ web-frontend/components/Hero.tsx
@@ -2,16 +2,16 @@ import Candlestick from '@/components/Candlestick';
 
 const Hero = () => (
     <div className="h-screen bg-black flex justify-end w-full text-white p-10 font-montserrat">
-        <div className="relative max-w-7xl flex-1 mr-10 text-white">
+        <div className="relative max-w-7xl sm:flex-1 sm:mr-10 text-white">
             <div className="absolute inset-y-1/4 transform -translate-y-20 z-10 max-w-lg">
                 <h1
-                    className="drop max-w-4xl text-6xl font-semibold"
+                    className="max-w-4xl text-4xl sm:text-6xl font-semibold"
                     style={{ lineHeight: '1.4' }}
                 >
                     AI powered news analysis.
                 </h1>
                 <p
-                    className="text-gray-300 text-3xl font-medium pt-7"
+                    className="text-zinc-300 text-xl sm:text-3xl font-medium pt-7"
                     style={{ lineHeight: '1.9' }}
                 >
                     A model for predicting the effect of news stories on stock
