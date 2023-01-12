23c9df81e79f777d2edcefa90573ceded5b1b9bf
alistairdivorty <alistair_divorty@hotmail.com>
2023-01-12 15:50:25 +0000

    Set up data fetching

--- web-frontend/context/articlesContext.tsx
+++ web-frontend/context/articlesContext.tsx
@@ -1,4 +1,5 @@
-import React, { useEffect } from 'react';
+import dayjs from 'dayjs';
+import React, { useEffect, useMemo } from 'react';
 import { ArticlesContextType, IArticle } from '@types';
 
 type Props = {
@@ -10,16 +11,25 @@ const ArticlesContext = React.createContext<ArticlesContextType | null>(null);
 const ArticlesContextProvider: React.FC<Props> = ({ children }) => {
     const [articles, setArticles] = React.useState<IArticle[]>([]);
 
+    const url: URL = useMemo(() => {
+        const url = new URL('/articles', 'https://api.stockoffthepress.com/');
+        url.searchParams.append(
+            'from',
+            dayjs().subtract(7, 'days').format('YYYY-MM-DD')
+        );
+        return url;
+    }, []);
+
     useEffect(() => {
         const fetchArticles = () => {
-            fetch('https://api.stockoffthepress.com/articles/')
+            fetch(url)
                 .then((res) => res.json())
                 .then((articles) => setArticles(articles))
                 .catch((err) => console.log('An error occured.'));
         };
 
         fetchArticles();
-    }, []);
+    }, [url]);
 
     return (
         <ArticlesContext.Provider value={{ articles }}>
--- web-frontend/types/index.ts
+++ web-frontend/types/index.ts
@@ -1,5 +1,12 @@
 export interface IArticle {
+    _id: { $oid: string };
+    date_published: { $date: string };
     headline: string;
+    description: string;
+    summary: string;
+    exchange: string;
+    symbol: string;
+    prediction: 'rise' | 'fall';
 }
 
 export type ArticlesContextType = {
